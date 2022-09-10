1193510db37e3a86d6ceae366ac32f3ff935a6dd
stas <79771234+ninahur1989@users.noreply.github.com>
2022-09-10 16:11:53 +0300

    console

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Messenger
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
